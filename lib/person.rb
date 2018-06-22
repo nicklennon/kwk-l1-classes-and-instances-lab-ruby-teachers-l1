c5cb901e6658aa1eb53f8141ea668b9b8a70c3cf
nicklennon <nicklennon.53@gmail.com>
2018-06-22 16:22:17 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,2 +1,14 @@
 # person.rb
 
+class Person
+  
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+  end
+  
+end
+
+adele_goldberg = Person.new("Adele Goldberg")
+alan_kay = Dog.new("Alan Kay")
