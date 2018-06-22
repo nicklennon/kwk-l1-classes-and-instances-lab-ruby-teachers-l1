2a1daa65b29e2a23566d79dcddb2c68519ab97fb
nicklennon <nicklennon.53@gmail.com>
2018-06-22 16:22:40 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
 # dog.rb
+
+class Dog
+  
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+  end
+  
+end
+
+fido = Dog.new("Fido")
+snoopy = Dog.new("Snoopy")
+lassie = Dog.new("Lassie")
+
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,14 @@
 # person.rb
+
+class Person
+  
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+  end
+  
+end
+
+adele_goldberg = Person.new("Adele Goldberg")
+alan_kay = Person.new("Alan Kay")
